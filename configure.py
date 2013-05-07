7341074b64aa6c06b7bb1a1d72e75b9cf993bb97
Daniele Esposti <expo@expobrain.net>
2013-05-08 00:26:14 +0100

    Removed configuration module

--- configure.py
+++ configure.py
@@ -102,7 +102,7 @@ class SkiaPreprocessor(object):
 
     @property
     def package_installs(self):
-        return ["skia_config.py", os.path.join(self.SIP_DIR, "__init__.py")]
+        return [os.path.join(self.SIP_DIR, "__init__.py")]
 
 
 if __name__ == "__main__":
@@ -131,23 +131,3 @@ if __name__ == "__main__":
     config = SkiaConfiguration(options.skia_include_dir, options.skia_libs_dir)
     preprocessor = SkiaPreprocessor()
     preprocessor.generate_code(config)
-
-    # Now we create the configuration module.  This is done by merging a Python
-    # dictionary (whose values are normally determined dynamically) with a
-    # (static) template.
-    content = {
-        # Publish where the SIP specifications for this module will be
-        # installed.
-        "skia_sip_dir": config.default_sip_dir,
-
-        # Publish the set of SIP flags needed by this module.  As these are the
-        # same flags needed by the qt module we could leave it out, but this
-        # allows us to change the flags at a later date without breaking
-        # scripts that import the configuration module.
-    #     "hello_sip_flags":  pyqt_sip_flags
-    }
-
-    # This creates the helloconfig.py module from the helloconfig.py.in
-    # template and the dictionary.
-    sipconfig.create_config_module(
-        "skia_config.py", "skia_config.py.in", content)
