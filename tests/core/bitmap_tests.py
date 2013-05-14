dc8d93e263281d3a4a7d9bc6345b7b34967ab699
Daniele Esposti <expo@expobrain.net>
2013-05-14 23:51:05 +0100

    Exposed methods with basic unit tests

--- tests/core/bitmap_tests.py
+++ tests/core/bitmap_tests.py
@@ -19,3 +19,47 @@ class SkBitmapTests(unittest.TestCase):
 
     def test_copy_constructor(self):
         self.assertIsInstance(SkBitmap(SkBitmap()), SkBitmap)
+
+    def test_empty(self):
+        self.assertTrue(SkBitmap().empty())
+
+    def test_is_null(self):
+        self.assertTrue(SkBitmap().isNull())
+
+    def test_config(self):
+        self.assertEqual(SkBitmap().config(), SkBitmap.kNo_Config)
+
+    def test_width(self):
+        self.assertEqual(SkBitmap().width(), 0)
+
+    def test_height(self):
+        self.assertEqual(SkBitmap().height(), 0)
+
+    @unittest.skip("FIXME")
+    def test_row_bytes(self):
+        self.assertEqual(SkBitmap().rowBytes(), 0)
+
+    def test_shift_per_pixel(self):
+        self.assertEqual(SkBitmap().shiftPerPixel(), 0)
+
+    def test_bytes_per_pixel(self):
+        self.assertEqual(SkBitmap().bytesPerPixel(), 0)
+
+    @unittest.skip("FIXME")
+    def test_row_bytes_as_pixels(self):
+        self.assertEqual(SkBitmap().rowBytesAsPixels(), 0)
+
+    @unittest.skip("FIXME")
+    def test_get_size(self):
+        self.assertEqual(SkBitmap().getSize(), 0)
+
+#     def test_get_size_64(self):
+#         self.assertIsInstance(SkBitmap().getSize64(), Sk64)
+
+    @unittest.skip("FIXME")
+    def test_get_safe_size(self):
+        self.assertEqual(SkBitmap().getSafeSize(), 0)
+
+    @unittest.skip("FIXME")
+    def test_get_pixels(self):
+        self.assertIsInstance(SkBitmap().getPixels(), bytearray)
