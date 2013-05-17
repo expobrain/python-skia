f0f12416f0e5e9448bb55a1630d1eaabf644522e
Daniele Esposti <expo@expobrain.net>
2013-05-17 20:21:37 +0100

    Exported public methods of SkClorTable class

--- tests/core/skcolortable_tests.py
+++ tests/core/skcolortable_tests.py
@@ -7,6 +7,51 @@ import unittest
 class SkColorTableTests(unittest.TestCase):
 
     @unittest.skip("FIXME")
-    def testInstance(self):
+    def test_instance(self):
         self.assertIsInstance(SkColorTable(16), SkColorTable)
         self.assertIsInstance(SkColorTable(SkColorTable(16)), SkColorTable)
+
+    def test_flags_enum(self):
+        self.assertTrue(hasattr(SkColorTable, "kColorsAreOpaque_Flag"))
+
+    @unittest.skip("FIXME")
+    def test_get_flags(self):
+        self.assertIsInstance(SkColorTable(16).getFlags(), int)
+
+    @unittest.skip("FIXME")
+    def test_is_opaque(self):
+        self.assertIsInstance(SkColorTable(16).isOpaque(), bool)
+
+    @unittest.skip("FIXME")
+    def test_count(self):
+        self.assertIsInstance(SkColorTable(16).count(), bool)
+
+    @unittest.skip("FIXME")
+    def test_lock_colors(self):
+        self.assertIsInstance(SkColorTable(16).lockColors(), int)
+
+    @unittest.skip("FIXME")
+    def test_lock_16_bit_cache(self):
+        self.assertIsInstance(SkColorTable(16).lock16BitCache(), int)
+
+    @unittest.skip("FIXME")
+    def test_unlock_colors(self):
+        try:
+            SkColorTable(16).unlockColors()
+            SkColorTable(16).unlockColors(False)
+        except Exception as e:
+            self.fail(e)
+
+    @unittest.skip("FIXME")
+    def test_unlock_16_bit_cache(self):
+        try:
+            SkColorTable(16).unlock16BitCache()
+        except Exception as e:
+            self.fail(e)
+
+    @unittest.skip("FIXME")
+    def test_set_flags(self):
+        t = SkColorTable(16)
+        t.setFlags(SkColorTable.kColorsAreOpaque_Flag)
+
+        self.assertEqual(t.getFlags(), SkColorTable.kColorsAreOpaque_Flag)
