52c1924fbbb963916336cf73b4eb96198dd12dc2
Daniele Esposti <expo@expobrain.net>
2013-05-20 23:13:13 +0100

    Exported SkCanvas constructors and destructor

--- tests/core/skcanvas_tests.py
+++ tests/core/skcanvas_tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from skia.core import SkCanvas
+from skia.core import SkCanvas, SkDevice, SkBitmap
 import unittest
 
 
@@ -8,6 +8,8 @@ class SkCanvasTests(unittest.TestCase):
 
     def test_instance(self):
         self.assertIsInstance(SkCanvas(), SkCanvas)
+        self.assertIsInstance(SkCanvas(SkDevice(SkBitmap())), SkCanvas)
+        self.assertIsInstance(SkCanvas(SkBitmap()), SkCanvas)
 
     def test_config888_enum(self):
         attrs = ("kNative_Premul_Config8888", "kNative_Unpremul_Config8888",
