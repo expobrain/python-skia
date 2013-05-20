467673dc9fdc16ddd9fc7de9ec6403c8f3a92d91
Daniele Esposti <expo@expobrain.net>
2013-05-20 23:09:50 +0100

    Added SkCanvas.Config8888 enumerator

--- tests/core/skcanvas_tests.py
+++ tests/core/skcanvas_tests.py
@@ -8,3 +8,11 @@ class SkCanvasTests(unittest.TestCase):
 
     def test_instance(self):
         self.assertIsInstance(SkCanvas(), SkCanvas)
+
+    def test_config888_enum(self):
+        attrs = ("kNative_Premul_Config8888", "kNative_Unpremul_Config8888",
+                 "kBGRA_Premul_Config8888", "kBGRA_Unpremul_Config8888",
+                 "kRGBA_Premul_Config8888", "kRGBA_Unpremul_Config8888")
+
+        for attr in attrs:
+            self.assertTrue(hasattr(SkCanvas, attr))
