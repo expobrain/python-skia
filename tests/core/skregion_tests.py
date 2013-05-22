9f5d07263ff2a5b5098fd9a9502cf1e04662804b
Daniele Esposti <expo@expobrain.net>
2013-05-22 18:40:20 +0100

    Export SkRegion.Op enumerator

--- tests/core/skregion_tests.py
+++ tests/core/skregion_tests.py
@@ -8,3 +8,10 @@ class SkRegionTests(unittest.TestCase):
 
     def test_instance(self):
         self.assertIsInstance(SkRegion(), SkRegion)
+
+    def test_config_enum(self):
+        attrs = ("kDifference_Op", "kIntersect_Op", "kUnion_Op", "kXOR_Op",
+                 "kReverseDifference_Op", "kReplace_Op")
+
+        for attr in attrs:
+            self.assertTrue(hasattr(SkRegion, attr))
